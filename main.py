9093fa95365f6f22ed0c95330beb7a302b2b4cae
abhiravjoshi <jabhirav@gmail.com>
2023-01-16 17:42:52 -0500

    Minor changes to last commit

--- main.py
+++ main.py
@@ -8,18 +8,21 @@ dfs = []
 for filepath in file_list:
     pdf = FPDF(orientation='p', unit='mm', format='A4')
     pdf.set_auto_page_break(auto=False)
+
     pdf.add_page()
     pdf.set_font(family='Times', size=16, style='b')
 
     filename = Path(filepath).stem
-    filename_split = filename.split('-')
+    invoice, date = filename.split('-')
 
-    invoice = f"Invoice # {filename_split[0]}"
-    date = f"Date {filename_split[1]}"
+    invoice_str = f"Invoice # {invoice}"
+    date_str = f"Date {date}"
 
-    pdf.cell(w=10, h=10, txt=invoice, align='L')
-    pdf.ln(10)
-    pdf.cell(w=10, h=10, txt=date, align='L')
+    pdf.cell(w=50, h=8, txt=invoice_str, align='L', ln=1)
+    # pdf.ln(10)
+    pdf.cell(w=50, h=8, txt=date_str, align='L')
+
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
     dfs.append(df)
+
     pdf.output(f"PDFs/{filename}.pdf")
